e4f848105ba340f3fb0b4429200a28eb5a2f2833
Dora He (Monash University) <dorah@monarch-login1.erc.monash.edu>
2020-11-12 21:06:57 +1100

    results

--- run.py
+++ run.py
@@ -13,12 +13,12 @@ algorithms[m_ogsa][m_after_fw] = f"{m_ogsa}_fw"
 
 
 num_repeat = 5
-# num_households_range = [50, 100, 500, 1000, 1500, 2000, 4000, 6000, 8000, 10000]
-num_households_range = [5000]
+num_households_range = [50, 100, 500, 1000, 1500, 2000, 4000, 6000, 8000, 10000]
+# num_households_range = [5000]
 penalty_weight_range = [1]
-num_tasks_dependent_range = [0, 3, 6, 9]
-num_full_flex_tasks_min = 10
-num_full_flex_tasks_max = 10
+num_tasks_dependent_range = [3]
+num_full_flex_tasks_min = 5
+num_full_flex_tasks_max = 8
 num_semi_flex_tasks = 0
 num_fixed_tasks = 0
 num_samples = 5
