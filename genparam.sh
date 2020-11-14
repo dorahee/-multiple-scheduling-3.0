720e7fe1f39c382fbb8c4c2a6fe39123298e738d
Dora He <dora.shan.he@gmail.com>
2020-11-15 01:46:59 +1100

    update plist

--- genparam.sh
+++ genparam.sh
@@ -4,15 +4,15 @@
 
 ### sh genparam.sh > PLIST
 
-# household == 10 cases (50, 100, 500, 1000, 2000, 4000, 6000, 8000, 10000, 15000)
+# household == 9 cases (50, 100, 500, 1000, 2000, 4000, 6000, 8000, 10000)
 # dependent ==  4 cases (0, 3, 6, 9)
-# penalty   == 10 cases (1, 10, 50, 100, 500, 1000, 5000)
+# penalty   == 5 cases (1, 1, 1, 1, 1)
 
-for h in 50 100 500 1000 2000 4000 6000 8000 10000 15000
+for h in 50 100 500 1000 2000 4000 6000 8000 10000
 do
   for d in 0 3 6 9
   do
-     for p in 1 10 50 100 500 1000 5000
+     for p in 1 1 1 1 1
      do
         echo "$h $d $p"
      done
