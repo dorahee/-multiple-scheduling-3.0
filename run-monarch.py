c5790945a194e30a6b6e098f41cd2679afee8429
Dora He (Monash University) <dorah@monarch-login1.erc.monash.edu>
2020-11-25 08:59:45 +1100

    r

--- run-monarch.py
+++ run-monarch.py
@@ -29,11 +29,11 @@ cpus_nums = 50
 ensure_dependent = True
 experiment_tracker = dict()
 timeout = None
-min_step_size = 0.0001
+min_step_size = 0
 ignore_tiny_step = False
 roundup_tiny_step = False
 print_done = False
-print_steps = False
+print_steps = True
 
 
 def main(num_households, num_tasks_dependent, penalty_weight, out, new_data=True, num_cpus=None, job_id=0):
